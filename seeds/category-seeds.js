9e21121190872c6369a7b775821970bee945e9e5
anagha778 <anagha778@gmail.com>
2021-03-18 14:41:10 -0400

    Updated models and seeds files to incorporate changes

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -2,12 +2,20 @@ const { Category } = require('../models');
 
 const categorydata = [
   {
-    category_name: 'Grocery',
-    category_desc: 'grocery'
+    category_name: 'Food',
+    category_desc: 'all food products belongs to this category'
   },
   {
-    category_name: 'Make-up',
-    category_desc: 'Makeup'
+    category_name: 'Medicine',
+    category_desc: 'all medicines belongs to this category'
+  },
+  {
+    category_name: 'Cosmetics',
+    category_desc: 'all cosmetics belongs to this category'
+  },
+  {
+    category_name: 'Books',
+    category_desc: 'all books belongs to this category'
   },
 ];
 
