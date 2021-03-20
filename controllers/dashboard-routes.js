868671f0934938c679faf8943e451bd2d27bd7cb
Allen <allenlovatt@gmail.com>
2021-03-20 14:54:36 -0400

    Dashboard Framework

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
-router.get('/', withAuth, (req, res) => {
+router.get('/dashboard', withAuth, (req, res) => {
     Post.findAll({
         where: {
         user_id: req.session.user_id
