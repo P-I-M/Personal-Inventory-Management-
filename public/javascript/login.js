26e18035a40bba6af77d8db5abdf3e8da4b263a8
dvicjon <devinvjones@gmail.com>
2021-03-22 20:11:24 -0400

    login edit

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,3 @@
-
 async function loginFormHandler(event) {
     event.preventDefault();
   
@@ -16,7 +15,7 @@ async function loginFormHandler(event) {
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
