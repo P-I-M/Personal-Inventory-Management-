3728a18530b8d3282a49bf8b0ca2c96d634d3776
anagha778 <anagha778@gmail.com>
2021-03-26 11:22:07 -0400

    added image upload to edit product

--- public/javascript/edit-product.js
+++ public/javascript/edit-product.js
@@ -1,7 +1,7 @@
 //Update product info
 async function handleGetCategorySubmit(event) {
-  event.preventDefault();
-    debugger;
+  //event.preventDefault();
+    
     let category_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 2
     ];
@@ -12,9 +12,12 @@ async function handleGetCategorySubmit(event) {
     id = parseInt(id);
     let mfg_date=null;
     let exp_date=null;
-      
+    let prod_desc=null;
+    let author_name=null;
+    const imageEl = document.getElementById("product-image");
+    
     const product_name = document.querySelector('input[name="product-title"]').value.trim();
-    const prod_desc = document.querySelector('input[name="prod-desc"]').value.trim();
+    prod_desc = document.querySelector('input[name="prod-desc"]').value.trim();
     const price = parseFloat(document.querySelector('input[name="price"]').value.trim());
     const stock = parseInt(document.querySelector('input[name="stock"]').value.trim());
     if(category_id == 1 || category_id== 2 || category_id == 3)
@@ -22,40 +25,79 @@ async function handleGetCategorySubmit(event) {
     mfg_date = Date.parse(document.querySelector('input[name="mfg_date"]').value.trim());
     exp_date = Date.parse(document.querySelector('input[name="exp_date"]').value.trim());
     }   
-    const author_name = document.querySelector('input[name="author_name"]').value.trim();
+    author_name = document.querySelector('input[name="author_name"]').value.trim();
 
-    if(product_name && price && stock)
-    {   
-      const response = await fetch(`/api/products/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-        product_name,
-        prod_desc,
-        price,
-        stock,
-        mfg_date,
-        exp_date,
-        author_name
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      window.alert("Product updated successfully!");
-      document.location.replace('/dashboard/products');
-    } else {
-      alert(response.statusText);
-    } 
+    if(document.body.contains(imageEl))
+    {
+      var img_url = imageEl.src;
+
+      if(product_name && price && stock)
+      {   
+        const response = await fetch(`/api/products/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+          img_url,
+          product_name,
+          prod_desc,
+          price,
+          stock,
+          mfg_date,
+          exp_date,
+          author_name
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    
+      if (response.ok) {
+        window.alert("Product updated successfully!");
+        document.location.replace('/dashboard/products');
+      } else {
+        alert(response.statusText);
+      } 
+    }
+    else
+    {
+      window.alert("Oops! Name, price and quantity are mandatory");
+    }
   }
-  else
-  {
-    window.alert("Oops! Name, price and quantity are mandatory");
-  }
+else
+{
+      if(product_name && price && stock)
+      {   
+        const response = await fetch(`/api/products/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+          product_name,
+          prod_desc,
+          price,
+          stock,
+          mfg_date,
+          exp_date,
+          author_name
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (response.ok) {
+        window.alert("Product updated successfully!");
+        document.location.replace('/dashboard/products');
+      } else {
+        alert(response.statusText);
+      } 
+    }
+    else
+    {
+      window.alert("Oops! Name, price and quantity are mandatory");
+    }
+}
+
 };
 
-document.querySelector('.edit-post-form').addEventListener('submit', handleGetCategorySubmit);
+document.querySelector('#update-product').addEventListener('click', handleGetCategorySubmit);
 
 
 
