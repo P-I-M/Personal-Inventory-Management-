e21bf5951c5a8df74e9da0e233051440c197bdaf
anagha778 <anagha778@gmail.com>
2021-03-27 16:01:53 -0400

    Daily suggestion logic

---
+++ public/javascript/daily-suggestion.js
@@ -0,0 +1,25 @@
+    var arr=[];
+    var flag = false;
+   
+    const dropdownEl = document.getElementById('person');
+        
+    for (let i =0;i<dropdownEl.length;i++)
+    {
+        //console.log(dropdownEl[i].text);
+        arr.push(dropdownEl[i].text);        
+    }
+   
+    var rndno = Math.floor((Math.random() * (arr.length-1)) + 0);
+    //window.alert(rndno);
+    var rndprod = arr[rndno];
+    var arr1 = arr[rndno].split("/");
+    if(arr1[0]==4)
+    {
+        arr1[3] = "N/A";
+    }
+
+    document.getElementById("nm").innerHTML = arr1[1];
+    document.getElementById("st").innerHTML = arr1[2];
+    document.getElementById("ex").innerHTML = arr1[3];
+    
+    
